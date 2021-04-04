225c6f3bcd5a3249bb029c5455a815c2a55b0c69
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-04-04 15:31:04 +0100

    Imported and rendered Homepage component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header';
+import HomePage from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <Header/>
+      <HomePage/>
     </>
   );
 }
