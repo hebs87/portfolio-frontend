7cb772973aa8d06be658565a83e08fe79e73597a
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-04-05 13:29:48 +0100

    Created getData API call in App.js to get all portfolio data when app initialises

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
+import {useState, useEffect} from 'react';
 import Header from './components/Header/Header';
 import HomePage from './pages/Home';
 import Footer from './components/Footer/Footer';
 
+import API from './utils/API';
+
 const App = () => {
+  const [details, setDetails] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const profile = await callApi('/personal/details');
+      const skills = await callApi('/personal/skills');
+      const projects = await callApi('/projects/details');
+      const experience = await callApi('/personal/experience');
+      const education = await callApi('/personal/education');
+      const details = {
+        profile: profile[0],
+        skills,
+        projects,
+        experience,
+        education
+      }
+      setDetails(details);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      // TODO: Forward to 404 erorr page once created
+      console.log(err);
+    }
+  };
+
+  const callApi = async (url) => {
+    const response = await API.get(url);
+    const {data} = response;
+    return data;
+  }
+
+  console.log(loading);
   return (
     <>
-      <Header/>
-      <HomePage/>
-      <Footer/>
+      {
+        loading &&
+        <h1>Loading</h1>
+      }
+      {
+        !loading && details &&
+          <>
+            <Header/>
+            <HomePage/>
+            <Footer/>
+          </>
+      }
     </>
   );
 }
