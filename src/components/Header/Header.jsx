146c071530c6545f7e6bc81b867f11c290c20969
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-04-05 03:04:57 +0100

    Resolved console errors in Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,23 +21,21 @@ const Header = () => {
                 <button className="navbar-toggler navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navigation" aria-controls="navigation" aria-expanded="false"
                         aria-label="Toggle navigation">
-                  <span className="navbar-toggler-bar bar1"></span>
-                  <span className="navbar-toggler-bar bar2"></span>
-                  <span className="navbar-toggler-bar bar3"></span>
+                  <span className="navbar-toggler-bar bar1"/>
+                  <span className="navbar-toggler-bar bar2"/>
+                  <span className="navbar-toggler-bar bar3"/>
                 </button>
               </div>
               <div className="collapse navbar-collapse justify-content-end" id="navigation">
                 <ul className="navbar-nav">
                   {
                     NAV_LINKS && NAV_LINKS.map(({id, text}) => (
-                      <>
-                        <li key={id} className="nav-item">
-                          <HeaderLink
-                            id={id}
-                            text={text}
-                          />
-                        </li>
-                      </>
+                      <li key={id} className="nav-item">
+                        <HeaderLink
+                          id={id}
+                          text={text}
+                        />
+                      </li>
                     ))
                   }
                 </ul>
